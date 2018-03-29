52fa87d08905325fd5110728bd2312343e914733
Salvatore Gonda <35118923+deemount@users.noreply.github.com>
2018-03-29 08:56:54 +0200

    Update and rename php/gzip_php2js.php to php/compression/gzip_php2js.php

---
+++ php/compression/gzip_php2js.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+* Setup compression level in zlib.
+* Notice: zlib needs to be installed in 
+*         your php configuration
+* 
+* Options: -1:
+*          1-9:
+*/
+ini_set('zlib.output_compression_level', 6);
+
+/*
+* Set content type for application
+*/
+header("Content-type: application/javascript");
+
+/*
+* Ensures only forced if the Accept-Encoding 
+* header contains "gzip", then call output
+* buffering with gz
+*/
+if(substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+{
+  header('Content-Encoding: gzip');
+  ob_start('ob_gzhandler');
+}
+
+/*
+* Put your javascript content here
+*/
+echo 'function Test()
+      {
+        return this;
+      };';
+
+/*
+* Get the buffered content from above
+*/
+$content = ob_get_contents();
+
+/*
+* Erase and turn off output buffering
+*/
+ob_end_clean();
+
+/*
+* Call again gz handler
+* Notice: Without calling again, content becomes 
+*         not compressed
+*/
+ob_start("ob_gzhandler");
+
+/*
+* Print buffered content
+*/
+echo $content;
+
+?>
