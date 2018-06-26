ee7c187ff127f049f61e24d8c249bd18f319dd6c
Salvatore Gonda <35118923+deemount@users.noreply.github.com>
2018-06-26 15:58:31 +0200

    Update reverseNumber.php

--- php/reverseNumber.php
+++ php/reverseNumber.php
@@ -1,16 +1,18 @@
 <?php
 
 $number = 5937429;
+
 function reverseNumber($number)
 {
     $result = 0;
     while($number > 0) 
     {
-        $temp = $number%10;
-        $result = $temp + $result * 10;
+        $result = ($result * 10) + $number % 10;
         $number = (int)($number/10);
     }
     return $result;
 }
+
+echo reverseNumber($number);
 
 ?>
