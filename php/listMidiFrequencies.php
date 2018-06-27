d22c92ec4c470617acf58a370044e850700fa9e2
Salvatore Gonda <35118923+deemount@users.noreply.github.com>
2018-06-27 16:00:49 +0200

    Update listMidiFrequencies.php

--- php/listMidiFrequencies.php
+++ php/listMidiFrequencies.php
@@ -1,13 +1,18 @@
 <?php
 
+/**
+ * Represents a list with midi notes from 21 up to 108 with their frequencies
+ */
+
+$start = 21; //do not change
+$end   = 108; //do not change
 $freq = 27.500; //A0 = 27.500 Hz
 $factor = pow(2,1/12); // pow calculates till 13 counts after comma (1.0594630943592953)
 $freq = $freq * $factor;
-$start = 21;
-$end   = 108;
+
 for($l=$start;$l<=$end;$l++)
 {
-    echo "Midi <b>$l</b>: $freq";
+    echo "Midi <b>$l</b>: $freq Hz";
     if($l>1)
         $freq *= $factor;
     echo '<br>';
